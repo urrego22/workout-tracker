f6d6d9a37c4d319efcb88073d606d3592660a0c9
Sara Garcia <urregosara984@gmail.com>
2025-10-01 22:17:26 -0500

    Commit 2: Implementación de rutas GET para la rama de reportes

--- src/controllers/reportes.controller.js
+++ src/controllers/reportes.controller.js
@@ -12,12 +12,19 @@ let reportes = [
 
 
 const getReportes = (req, res) => {
-  
+   res.status(200).json(reportes);
 };
 
 
 const getReporteByUsuario = (req, res) => {
-  
+const { usuarioId } = req.params;
+  const reporte = reportes.find(r => r.usuarioId == usuarioId);
+
+  if (!reporte) {
+    return res.status(404).json({ error: "Reporte no encontrado" });
+  }
+
+  res.status(200).json(reporte);
 };
 
 
